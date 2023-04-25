1631e72018677f858d748c98027af70d3128e5da
Riley Allred <riley_allred@hotmail.com>
2023-04-25 13:45:19 -0600

    worked on lot of todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1)
+  const result = await db
+  .transaction('jate','readwrite')
+  .objectStore('jate')
+  .put({id: 0,content})
+  console.log(result)
+  return result
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate',1)
+  const result = await db
+  .transaction('jate','readonly')
+  .objectStore('jate')
+  .getAll()
+  return result
+};
 
 initdb();
